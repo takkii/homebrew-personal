54bb94bbc970bc2223b7a3d5ac0e067ff98deaef
Takayuki Kamiyama <karuma.reason@gmail.com>
2019-04-29 21:28:56 +0900

    Update
    version up.

--- zinbei2.rb
+++ zinbei2.rb
@@ -6,7 +6,7 @@ class Zinbei2 < Formula
   url "https://github.com/takkii/zinbei2/archive/2.0.4.tar.gz"
   sha256 "4fc0c93c247f858a955b7d68ed36a79bd7334c89ed2ff152bf48465be3627d25"
   head "https://github.com/takkii/zinbei2.git"
-  version "2.0.3"
+  version "2.0.4"
 
   def install
    bin.install Dir['bin/*']
